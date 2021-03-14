b04999223aea9f16f86b8bdd2593b04dd55039a0
gozde arslan <garslan@myseneca.ca>
2021-03-14 18:51:13 -0400

    corrected spellign error

--- server.js
+++ server.js
@@ -21,11 +21,7 @@ app.set("view engine", "handlebars");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 const sgMail = require("@sendgrid/mail");
-
-
-
-
-//Test
+require("dotenv").config({ path: "config/keys.env" });
 
 //////////////////////////////// ----ROUTES----///////////////////////////////////
 //HOME PAGE//
@@ -50,82 +46,39 @@ app.get("/sign", (req, res) => {
   });
 });
 app.post("/sign", (req, res) => {
-
- 
-  if (req.body.firstName ==="") {
+  if (req.body.firstName == "" || req.body.firstName.length < 2) {
     error1 = "*First name must be  between 2 and 30 character long";
     res.render("sign", {
       title: "Sign In",
       error1: error1,
+      
     });
-  }
-  else if (req.body.lastName ==="") {
+  } else if (req.body.lastName === "" || req.body.lastName.length < 2) {
     error2 = "*Last name must be between 2 and 30 character long";
     res.render("sign", {
       title: "Sign In",
       error2: error2,
+     
     });
-  }   
+  }
 
-   var emailControl = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9.-]+(\.[a-zA-Z0-9_-]+)$/;
+  var emailControl = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9.-]+(\.[a-zA-Z0-9_-]+)$/;
 
   if (!emailControl.test(req.body.email)) {
     error3 = "*Please enter your email address";
     res.render("sign", {
       title: "sign In",
       error3: error3,
+      
     });
   }
- var passwordControl = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*){8,}/; 
- if (!passwordControl.test(req.body.psw)) {
+  var passwordControl = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*){8,}/;
+  if (!passwordControl.test(req.body.psw) ||req.body.psw.length == "") {
     error5 = "*Please enter valid password";
     res.render("sign", {
       title: "sign In",
       error5: error5,
-    });
-  } 
-  
-  else {
-    sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
-    const msg = {
-      to: "gozdearslan2010@gmail.com", // Change to your recipient
-      from: "garslan@myseneca.ca", // Change to your verified sender
-      subject: "MooviE-x Registration Information",
-      text: "and easy to do anywhere, even with Node.js",
-      html: "<strong> Account Successfully Created </strong>",
-    };
-    sgMail
-      .send(msg)
-      .then(() => {
-        console.log("Email sent");
-        res.redirect("/welcome");
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-    }
-  
- });
-
-//---LOGIN---//
-app.get("/login", (req, res) => {
-  //load about.handlebars
-
-  res.render("login", {});
-});
-app.post("/login", (req, res) => {
-  if (req.body.email === "") {
-    error1 = "*Please enter valid email";
-    res.render("login", {
-      title: "log In",
-      error1: error1,
-    });
-  }
-  else if (req.body.psw === "") {
-    error2 = "*Password must be 8 character length";
-    res.render("login", {
-      title: "Log In",
-      error2: error2,
+     
     });
   } else {
     sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
@@ -145,8 +98,47 @@ app.post("/login", (req, res) => {
       .catch((error) => {
         console.error(error);
       });
-    }
-  
+  }
+});
+
+//---LOGIN---//
+app.get("/login", (req, res) => {
+  //load about.handlebars
+
+  res.render("login", {});
+});
+app.post("/login", (req, res) => {
+  if (req.body.email === "") {
+    error1 = "*Please enter valid email";
+    res.render("login", {
+      title: "log In",
+      error1: error1,
+    });
+  } else if (req.body.psw === "") {
+    error2 = "*Password must be 8 character length";
+    res.render("login", {
+      title: "Log In",
+      error2: error2,
+    });
+  } else {
+    sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
+    const msg = {
+      to: "gozdearslan2010@gmail.com", // Change to your recipient
+      from: "garslan@myseneca.ca", // Change to your verified sender
+      subject: "MooviE-x Registration Information",
+      text: "and easy to do anywhere, even with Node.js",
+      html: "<strong> You have successfully login</strong>",
+    };
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log("Email sent");
+        res.redirect("/");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 });
 
 app.get("/payment", (req, res) => {
@@ -168,8 +160,6 @@ app.post("/contact", (req, res) => {
 app.get("/welcome", (req, res) => {
   res.render("welcome", {
     title: "Welcome",
-
-    
   });
 });
 
